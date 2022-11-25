6d930c54da4995b2393f4b2a120ac8da1090ea60
mraafayv <raafayvohra.mrv@gmail.com>
2022-11-25 11:31:56 +0500

    added user input functionality

--- app.js
+++ app.js
@@ -1,105 +1,117 @@
-let input = 3;
+// let input = 3;
+let input = prompt("Enter the size of cube: ");
+
 let requiredResult = [];
-let counter = 1;
+if (input % 2 !== 0) {
+    let counter = 1;
 
-let mainContainer = document.querySelector('.main-container');
+    let mainContainer = document.querySelector('.main-container');
 
-//creates an array according to the size provided by the user
-for (let size = 0; size < input; size++) {
-    requiredResult.push(Array());
-    let rows = document.createElement('div');
-    rows.className = 'row';
+    //creates an array according to the size provided by the user
+    for (let size = 0; size < input; size++) {
+        requiredResult.push(Array());
+        let rows = document.createElement('div');
+        rows.className = 'row';
 
-    for (let j = 0; j < input; j++) {
-        let square = document.createElement('div');
-        square.className = 'square button-20';
+        for (let j = 0; j < input; j++) {
+            let square = document.createElement('div');
+            square.className = 'square button-20';
 
-        rows.append(square);
+            rows.append(square);
 
+        }
+        mainContainer.append(rows);
+        // console.log(rows);
     }
-    mainContainer.append(rows);
-    // console.log(rows);
-}
-// mainContainer.append(square)
+    // mainContainer.append(square)
 
 
 
-function placeNumbers() {
+    function placeNumbers() {
 
 
-    let number = 1;
+        let number = 1;
 
-    //determines the middle point to start the insertion from
-    let middle = (requiredResult.length - 1) / 2;
-
-
-    let i = 0;
-    let j = middle;
-
-    // console.log(i,j);
-
-    requiredResult[i][j] = number;
-    mainContainer.children[i].children[j].innerText = number;
-    
-    number++;
-
-    //inserts the numbers into the cube according to the given size
-    for (let k = 0; k < input ** 2 - 1; k++) {
-        setTimeout(() => {
-            let prevRow = i;
-            let prevCol = j;
+        //determines the middle point to start the insertion from
+        let middle = (requiredResult.length - 1) / 2;
 
 
-            if (i === 0) {
-                i += (input - 1);
-            } else {
-                i -= 1;
-            }
-
-
-            if (j === input - 1) {
-                j -= (input - 1);
-            } else {
-                j += 1;
-            }
-            // console.log(i,j);
-
-
-            if (typeof requiredResult[i][j] === 'undefined') {
-                requiredResult[i][j] = number;
-                mainContainer.children[i].children[j].innerText = number;
-                number++;
-            } else {
-
-                i = prevRow;
-                j = prevCol;
-                i++;
-                requiredResult[i][j] = number;
-                mainContainer.children[i].children[j].innerText = number;
-                number++;
-            }
-        }, counter * 1000)
-        counter += 1;
-
-        //checks if there is a number present at the location or not
-
+        let i = 0;
+        let j = middle;
 
         // console.log(i,j);
 
-    }
+        requiredResult[i][j] = number;
+        mainContainer.children[i].children[j].innerText = number;
 
+        number++;
+
+        //inserts the numbers into the cube according to the given size
+        for (let k = 0; k < input ** 2 - 1; k++) {
+            setTimeout(() => {
+                let prevRow = i;
+                let prevCol = j;
+
+
+                if (i === 0) {
+                    i += (input - 1);
+                } else {
+                    i -= 1;
+                }
+
+
+                if (j === input - 1) {
+                    j -= (input - 1);
+                } else {
+                    j += 1;
+                }
+                // console.log(i,j);
+
+
+                //checks if there is a number present at the location or not
+                if (typeof requiredResult[i][j] === 'undefined') {
+                    requiredResult[i][j] = number;
+                    mainContainer.children[i].children[j].innerText = number;
+                    number++;
+                } else {
+
+                    i = prevRow;
+                    j = prevCol;
+                    i++;
+                    requiredResult[i][j] = number;
+                    mainContainer.children[i].children[j].innerText = number;
+                    number++;
+                }
+            }, counter * 1000)
+            counter += 1;
+
+
+
+            // console.log(i,j);
+
+        }
+
+    }
+} else {
+    alert("please enter an odd number (3 or 5 or 9)");
+    location.href = 'http://127.0.0.1:5500/index.html';
+    // input = prompt("Enter the size of cube: ");
 }
 
-console.table(requiredResult);
+
+
+
+
+// console.table(requiredResult);
 
 
 placeNumbers();
 
 //Checking sum of row for verifying the result
-let sum = 0;
-for (let x = 0; x < input; x++) {
-    sum += requiredResult[0][x];
+// let sum = 0;
+// for (let x = 0; x < input; x++) {
+//     sum += requiredResult[0][x];
 
-}
+// }
 
-console.log("Sum: " + sum);
+// console.log("Sum: " + sum);
